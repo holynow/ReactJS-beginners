1af70577d5aea59d826acb874391597de526a0f2
sindy970 <sindy970@dooribun.com>
2023-09-26 17:23:27 +0900

    '리액트'

--- react-for/src/App.js
+++ react-for/src/App.js
@@ -1,13 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+
+import { useState,useEffect } from "react";
+
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyWord] = useState("")
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyWord(event.target.value);
+  useEffect(()=>{
+    console.log("i run only once.");
+  }, []);
+  useEffect(()=> {
+      console.log("i run when 'keyword' changes");
+  },[keyword]);
+  useEffect(()=> {
+      console.log("i run when 'counter' changes");
+  },[counter]);
+  useEffect(()=> {
+      console.log("i run when 'counter' and 'keyword' changes");
+  },[counter, keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Confirm"} />
+      <input onChange={onChange} type="text" placeholder="Search"></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
-}
+  }
 
 export default App;
