70029c0f8c41b91b5b2b58d60f2e3ae3f857f4cc
sindy970 <sindy970@dooribun.com>
2023-10-06 17:03:42 +0900

    '리액트라우터돔'

--- react-for/src/App.js
+++ react-for/src/App.js
@@ -1,25 +1,20 @@
-import { useEffect, useState } from "react";
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers").then(response => response.json()).then(json => {
-      setCoins(json);
-      setLoading(false);
-    });
-  },[]);
-  return(
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : 
-      <select>
-        {coins.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
-      </select>
-      }
-      
-    </div>
-  )
+  return (
+    <Router>
+      <Switch>
+        <Route path="/Movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
