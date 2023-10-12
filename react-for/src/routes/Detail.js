62d27b105e130c7e00f5848d47e66e3c38a1ba56
sindy970 <sindy970@dooribun.com>
2023-10-12 15:23:40 +0900

    '영화추천사이트제작'

--- react-for/src/routes/Detail.js
+++ react-for/src/routes/Detail.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [detail, setDetail] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    const getDetail = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setDetail(json.data.movie);
+      setLoading(false);
+    };
+    getDetail();
+  }, [id]);
+  return <div>{loading ? <h1>Loading...</h1> : <h1>Detail</h1>}</div>;
 }
 
 export default Detail;
